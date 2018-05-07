5952376547d55449d33ddb6e71fee293bd29b323
akun <guikun@cashubs.com>
2018-05-07 22:49:08 +0800

    class loader test

---
+++ src/main/java/com/gk/classload/ClassLoadTest.java
@@ -0,0 +1,42 @@
+package com.gk.classload;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import sun.misc.Launcher;
+
+import java.net.URL;
+
+/**
+ * Created by akun on 2018/5/7.
+ */
+@Slf4j
+public class ClassLoadTest {
+
+    @Getter
+    @Setter
+    private int age;
+
+    @Test
+    public void ClassLoadTreeTest() {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        while (classLoader != null) {
+            log.info("clzz loader:{}", classLoader.getClass());
+            classLoader = classLoader.getParent();
+        }
+        URL[] urLs = Launcher.getBootstrapClassPath().getURLs();
+        for (URL url : urLs) {
+            log.info("url:{}", url.getPath());
+        }
+    }
+
+    @Test
+    public void loadClassByClassMethod() throws Exception {
+        Class<?> name = Class.forName("com.gk.classload.ClassLoadTest", true, this.getClass().getClassLoader());
+        ClassLoadTest o = (ClassLoadTest)name.newInstance();
+        o.setAge(1);
+        log.info("age:{}", o.getAge());
+
+    }
+}
