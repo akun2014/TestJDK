01b4a82a466c567a167ad49dc606494c8424717f
guikun <guikun@cashbus.com>
2019-02-22 18:29:42 +0800

    BeanDefinitionReader test

---
+++ src/main/java/com/gk/BeanDefinitionReaderTest.java
@@ -0,0 +1,37 @@
+package com.gk;
+
+import com.gk.bean.User;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.support.StaticApplicationContext;
+
+/**
+ * Created by akun on 2019/2/22.
+ *
+ * @see XmlBeanDefinitionReader
+ * @see PropertiesBeanDefinitionReader
+ */
+@Slf4j
+public class BeanDefinitionReaderTest {
+    StaticApplicationContext applicationContext = new StaticApplicationContext();
+
+    @Test
+    public void testXmlBeanDefinitionReader() {
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(applicationContext);
+        reader.loadBeanDefinitions("application.xml");
+
+        User user = (User) applicationContext.getBean("user");
+        System.out.println(user.toString());
+    }
+
+    @Test
+    public void test() {
+        PropertiesBeanDefinitionReader reader = new PropertiesBeanDefinitionReader(applicationContext);
+        reader.loadBeanDefinitions("application.properties");
+
+        User user = (User) applicationContext.getBean("user");
+        System.out.println(user.toString());
+    }
+}
