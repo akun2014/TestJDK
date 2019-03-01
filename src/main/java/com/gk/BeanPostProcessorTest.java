51a7e317736fc3423ff2a8a300848430ab742ef4
akun <e-gk@qq.com>
2019-03-02 00:59:13 +0800

    BeanPostProcessorTest

---
+++ src/main/java/com/gk/BeanPostProcessorTest.java
@@ -0,0 +1,25 @@
+package com.gk;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by akun on 2019/2/28.
+ */
+@Slf4j
+@Component
+public class BeanPostProcessorTest implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        log.info("Before-{} bean:{}", beanName, bean);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        log.info("After-{} bean:{}", beanName, bean);
+        return bean;
+    }
+}
