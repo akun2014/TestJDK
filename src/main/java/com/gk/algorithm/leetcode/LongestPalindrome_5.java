490b28371f15ed1bc601d02db43767067d811353
akun <e-gk@qq.com>
2019-03-09 14:17:19 +0800

    #3,4,5,7

---
+++ src/main/java/com/gk/algorithm/leetcode/LongestPalindrome_5.java
@@ -0,0 +1,23 @@
+package com.gk.algorithm.leetcode;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * Created by akun on 2019/3/9.
+ *
+ *@see <a href="https://zh.wikipedia.org/wiki/动态规划">动态规划问题</a>
+ */
+@Slf4j
+public class LongestPalindrome_5 {
+
+    @Test
+    public void test() throws Exception {
+        longestPalindrome("");
+    }
+
+    public String longestPalindrome(String s) {
+
+        return s;
+    }
+}
