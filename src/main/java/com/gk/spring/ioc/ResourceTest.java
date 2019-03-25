5001c0a76615e85ea6b7aa5f910f3c69656fd4b1
akun <e-gk@qq.com>
2019-03-25 10:35:14 +0800

    Resource and ResourceLoader

--- src/main/java/com/gk/spring/ioc/ResourceTest.java
+++ src/main/java/com/gk/spring/ioc/ResourceTest.java
@@ -2,9 +2,9 @@ package com.gk.spring.ioc;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.*;
+import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
 import org.springframework.web.context.support.ServletContextResource;
 
 import java.io.File;
@@ -68,5 +68,22 @@ public class ResourceTest {
 
     public void testServletContextResource() {
         ServletContextResource resource = new ServletContextResource(null, "");
+    }
+
+    private ResourceLoader resourceLoader = new DefaultResourceLoader();
+
+    @Test
+    public void test() throws Exception {
+        Resource resource = resourceLoader.getResource("logback.xml");
+        Assert.isTrue("DefaultResourceLoader.ClassPathContextResource".equals(ClassUtils.getShortName(resource.getClass())), "");
+
+        Resource resource1 = resourceLoader.getResource("/Users/boyu/ioc.png");
+        Assert.isTrue("DefaultResourceLoader.ClassPathContextResource".equals(ClassUtils.getShortName(resource1.getClass())), "");
+
+        Resource resource2 = resourceLoader.getResource("file:/Users/boyu/ioc.png");
+        Assert.isInstanceOf(FileUrlResource.class, resource2);
+
+        Resource resource3 = resourceLoader.getResource("classpath:/Users/boyu/ioc.png");
+        Assert.isInstanceOf(ClassPathResource.class, resource3);
     }
 }
