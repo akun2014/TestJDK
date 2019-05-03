180840568b7e63b7608c526a92ecc264addea54d
akun <e-gk@qq.com>
2019-05-04 04:28:09 +0800

    environment test

---
+++ src/main/java/com/gk/spring/EnvironmentAndProperties/EnvironmentTest.java
@@ -0,0 +1,25 @@
+package com.gk.spring.EnvironmentAndProperties;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.PropertyResolver;
+import org.springframework.core.env.PropertySourcesPropertyResolver;
+
+/**
+ * @author akun
+ * @date 2019-04-14
+ */
+@Slf4j
+public class EnvironmentTest {
+
+    Environment environment;
+
+    @Test
+    public void test() throws Exception {
+//        environment.acceptsProfiles(null);
+
+//        PropertyResolver propertyResolver = new PropertySourcesPropertyResolver(propertySources);
+    }
+
+}
---
+++ src/test/java/com/gk/spring/EnvironmentTest.java
@@ -0,0 +1,25 @@
+package com.gk.spring;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author akun
+ * @date 2019-04-14
+ */
+@Slf4j
+@RunWith(SpringRunner.class)
+public class EnvironmentTest {
+
+    @Autowired
+    Environment environment;
+
+    @Test
+    public void test() throws Exception {
+        environment.getActiveProfiles();
+    }
+}
