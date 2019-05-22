25dbcc6085c023d5995a213d5c141938a029d28c
akun <e-gk@qq.com>
2019-05-22 09:50:18 +0800

    去掉不必要的public修饰

--- src/main/java/com/gk/proxy/aop/TicketService.java
+++ src/main/java/com/gk/proxy/aop/TicketService.java
@@ -4,12 +4,18 @@ package com.gk.proxy.aop;
  * Created by akun on 2018/12/20.
  */
 public interface TicketService {
-    //售票
-    public void sellTicket();
+    /**
+     * 售票
+     */
+    void sellTicket();
 
-    //问询
-    public void inquire();
+    /**
+     * 问询
+     */
+    void inquire();
 
-    //退票
-    public void withdraw();
+    /**
+     * 售票
+     */
+    void withdraw();
 }
