17e1ae406e7f6c93bd0f9c8e84bb5a64329aa319
akun <e-gk@qq.com>
2019-05-29 01:23:08 +0800

    warmup ratelimiter

--- src/main/java/com/ownerkaka/testjdk/guava/RateLimiterTest.java
+++ src/main/java/com/ownerkaka/testjdk/guava/RateLimiterTest.java
@@ -13,19 +13,19 @@ import java.util.concurrent.*;
  */
 @Slf4j
 public class RateLimiterTest {
-    private final RateLimiter limiter = RateLimiter.create(5);
+    private final RateLimiter limiter = RateLimiter.create(20, 90, TimeUnit.SECONDS);
     private ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat("rateLimiter-pool-%d").build();
 
     private ExecutorService threadPool = new ThreadPoolExecutor(4, 200,
             0L, TimeUnit.MILLISECONDS,
-            new LinkedBlockingQueue<>(1024), threadFactory, new ThreadPoolExecutor.AbortPolicy());
+            new LinkedBlockingQueue<>(), threadFactory, new ThreadPoolExecutor.AbortPolicy());
 
 //    ExecutorService threadPool = Executors.newFixedThreadPool(2);
 
 
     @Test
     public void test() throws Exception {
-        int masTaskNum = 50;
+        int masTaskNum = 50000;
         for (int i = 0; i < masTaskNum; i++) {
             Runnable task = this::rateLimit;
             threadPool.submit(task);
@@ -38,6 +38,10 @@ public class RateLimiterTest {
     private void rateLimit() {
         long threadId = Thread.currentThread().getId();
         limiter.acquire();
+//        try {
+//            TimeUnit.MILLISECONDS.sleep(100);
+//        } catch (InterruptedException ignore) {
+//        }
         log.info("try acquire done threadId:{}", threadId);
     }
 }
