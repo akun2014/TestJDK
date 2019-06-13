95dd322b9264d27f55620c99029ddd6e040bd81d
akun <guikun@cashbus.com>
2019-06-13 17:28:16 +0800

    deadlock update

--- src/main/java/com/ownerkaka/testjdk/deadlock/DeadLockTest.java
+++ src/main/java/com/ownerkaka/testjdk/deadlock/DeadLockTest.java
@@ -1,33 +1,38 @@
 package com.ownerkaka.testjdk.deadlock;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by akun on 2018/5/6.
  */
 public class DeadLockTest {
 
+    private static final String lock1 = "lock1";
+    private static final String lock2 = "lock2";
+
     public static void main(String[] args) {
-
-        final String lock1 = "lock1";
-        final String lock2 = "lock2";
-
         Thread thread1 = new Thread(() -> {
-            while (true) {
-                synchronized (lock1) {
-                    System.out.println("got lock1");
-                    synchronized (lock2) {
-                        System.out.println("got lock2");
-                    }
+            synchronized (lock1) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(500L);
+                } catch (InterruptedException ignore) {
+                }
+                System.out.println("got lock1");
+                synchronized (lock2) {
+                    System.out.println("got lock2");
                 }
             }
         });
 
         Thread thread2 = new Thread(() -> {
-            while (true) {
-                synchronized (lock2) {
-                    System.out.println("got lock2");
-                    synchronized (lock1) {
-                        System.out.println("got lock1");
-                    }
+            synchronized (lock2) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(500L);
+                } catch (InterruptedException ignore) {
+                }
+                System.out.println("got lock2");
+                synchronized (lock1) {
+                    System.out.println("got lock1");
                 }
             }
         });
