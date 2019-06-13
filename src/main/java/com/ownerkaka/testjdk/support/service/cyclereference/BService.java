037669e904ef175ab1c0a7125ae93fc17ac18e5c
akun <guikun@cashbus.com>
2019-06-13 11:00:29 +0800

    cycle reference

---
+++ src/main/java/com/ownerkaka/testjdk/support/service/cyclereference/BService.java
@@ -0,0 +1,31 @@
+package com.ownerkaka.testjdk.support.service.cyclereference;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author akun
+ * @since 2019-06-12
+ */
+@Slf4j
+@Service
+public class BService implements BeanNameAware {
+
+    @Autowired
+    CService cService;
+
+    public void test() {
+        log.info("test method in {}", name);
+    }
+
+    @Getter
+    private String name;
+
+    @Override
+    public void setBeanName(String name) {
+        this.name = name;
+    }
+}
