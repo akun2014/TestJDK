9b060632554eda92d0e57f171c6726cefbeb326e
akun <guikun@cashbus.com>
2019-07-13 11:59:42 +0800

    RequestMappingHandlerMapping

---
+++ src/main/java/com/ownerkaka/testjdk/spring/web/RequestMappingHandlerMappingTests.java
@@ -0,0 +1,21 @@
+package com.ownerkaka.testjdk.spring.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+/**
+ * @author akun
+ * @since 2019-07-10
+ */
+@Slf4j
+public class RequestMappingHandlerMappingTests {
+
+    private final RequestMappingHandlerMapping handlerMapping = new RequestMappingHandlerMapping();
+
+    @Test
+    public void test() {
+
+//        handlerMapping.getPathMatcher()
+    }
+}
