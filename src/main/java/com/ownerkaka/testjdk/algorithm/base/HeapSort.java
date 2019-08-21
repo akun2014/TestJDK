add092b93f702129771909505049e253400ba4e0
akun <guikun@cashbus.com>
2019-08-21 15:55:44 +0800

    HeapSort

---
+++ src/main/java/com/ownerkaka/testjdk/algorithm/base/HeapSort.java
@@ -0,0 +1,61 @@
+package com.ownerkaka.testjdk.algorithm.base;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author akun
+ * @since 2019-08-21
+ * 堆排序算法
+ */
+@Slf4j
+public class HeapSort {
+
+
+    @Test
+    public void test() {
+        int[] arr = {9, 8, 7, 6, 10, 5, 4, 3, 2, 1};
+        sort(arr);
+        log.info("{}", Arrays.toString(arr));
+    }
+
+
+    private static void sort(int[] arr) {
+        //1.构建大顶堆
+        for (int i = arr.length / 2 - 1; i >= 0; i--) {
+            //从第一个非叶子结点从下至上，从右至左调整结构
+            adjustHeap(arr, i, arr.length);
+        }
+        //2.调整堆结构+交换堆顶元素与末尾元素
+        for (int j = arr.length - 1; j > 0; j--) {
+            swap(arr, 0, j);//将堆顶元素与末尾元素进行交换
+            adjustHeap(arr, 0, j);//重新对堆进行调整
+        }
+
+    }
+
+    private static void adjustHeap(int[] arr, int i, int length) {
+        int temp = arr[i];//先取出当前元素i
+        for (int k = i * 2 + 1; k < length; k = k * 2 + 1) {//从i结点的左子结点开始，也就是2i+1处开始
+            if (k + 1 < length && arr[k] < arr[k + 1]) {//如果左子结点小于右子结点，k指向右子结点
+                k++;
+            }
+            if (arr[k] > temp) {//如果子节点大于父节点，将子节点值赋给父节点（不用进行交换）
+                arr[i] = arr[k];
+                i = k;
+            } else {
+                break;
+            }
+        }
+        arr[i] = temp;//将temp值放到最终的位置
+    }
+
+
+    private static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
